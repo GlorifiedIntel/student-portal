3bd65bc36825f5dbc54871717015c2a07eca422a
kcminchakpu <165849310+GlorifiedIntel@users.noreply.github.com>
2025-08-21 18:08:40 +0100

    commit

--- .history/src/app/(dashboard)/layout_20250821170458.jsx
+++ .history/src/app/(dashboard)/layout_20250821170458.jsx
@@ -1,15 +1,13 @@
 import Menu from "@/components/Menu";
 import styles from "./Layout.module.css";
-import Navbar from "@/components/Navbar";
 
 export default function DashboardLayout({ children }) {
   return (
     <div className={styles.dashboardlayout}>
-      <Menu />
-      <main className={styles.dashboardcontent}>
-        <Navbar />
-        {children}
-        </main>
+      <aside className={styles.sidebar}>
+        <Menu />
+      </aside>
+      <main className={styles.dashboardcontent}>{children}</main>
     </div>
   );
 }
--- .history/src/app/(dashboard)/layout_20250821172659.jsx
+++ .history/src/app/(dashboard)/layout_20250821172659.jsx
@@ -1,13 +1,11 @@
 import Menu from "@/components/Menu";
 import styles from "./Layout.module.css";
-import Navbar from "@/components/Navbar";
 
 export default function DashboardLayout({ children }) {
   return (
     <div className={styles.dashboardlayout}>
       <Menu />
       <main className={styles.dashboardcontent}>
-        <Navbar />
         {children}
         </main>
     </div>
