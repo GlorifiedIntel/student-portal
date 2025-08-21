fcc6ed4461ad7124c75ac019bc331888fa906c5a
shajidnursingcollege-svg <165849310+GlorifiedIntel@users.noreply.github.com>
2025-08-21 18:06:10 +0100

    commit

--- src/app/(dashboard)/layout.jsx
+++ src/app/(dashboard)/layout.jsx
@@ -1,11 +1,15 @@
 import Menu from "@/components/Menu";
 import styles from "./Layout.module.css";
+import Navbar from "@/components/Navbar";
 
 export default function DashboardLayout({ children }) {
   return (
     <div className={styles.dashboardlayout}>
       <Menu />
-      <main className={styles.dashboardcontent}>{children}</main>
+      <main className={styles.dashboardcontent}>
+        <Navbar />
+        {children}
+      </main>
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import { Search, MessageSquare, Megaphone, User } from "lucide-react";
+import styles from "./Navbar.module.css";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div className={styles.navbar}>
+      {/* SEARCH BAR */}
+      <div className={styles.searchBar}>
+        <Search size={14} className={styles.iconInline} />
+        <input 
+          type="text" 
+          placeholder="Search..." 
+          className={styles.searchInput} 
+        />
+      </div>
 
-export default Navbar
+      {/* ICONS AND USER */}
+      <div className={styles.iconUserWrapper}>
+        <div className={styles.icon}>
+          <MessageSquare size={20} />
+        </div>
+
+        <div className={`${styles.icon} ${styles.relativeIcon}`}>
+          <Megaphone size={20} />
+          <div className={styles.badge}>1</div>
+        </div>
+
+        <div className={styles.userInfo}>
+          <span className={styles.userName}>John Doe</span>
+          <span className={styles.userRole}>Admin</span>
+        </div>
+
+        <div className={styles.avatar}>
+          <User size={20} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
